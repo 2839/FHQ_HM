b85f78fcf8cd43e90e6cb14ebbd9b8fb5e6eb23e
fhq <fhq5566@126.com>
2015-09-22 14:48:51 +0800

    添加项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by 冯华强 on 15/9/22.
+//  Copyright (c) 2015年 HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by 冯华强 on 15/9/22.
+//  Copyright (c) 2015年 HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
